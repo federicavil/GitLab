1019b46d7f98e9f61ee45607a4b6f6b9e08dbc40
Federica <federica_vil@yahoo.it>
2022-04-20 09:53:18 +0200

    retrieve commit code added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello world");
-        retrieveCommit("added");
+        try {
+            retrieveCommit("C:/Users/Federica/git/GitLab","added");
+        }catch(Exception e) {
+            System.out.println("Exception");
+        }
     }
 
-    public static void retrieveCommit(String string){
+    public static void retrieveCommit(String path, String toSearch) throws Exception {
+                ProcessBuilder builder = new ProcessBuilder(
+                        "cmd.exe", "/c", "cd " + path+ " && git log --grep="+toSearch+" --date=iso-strict" );
+                builder.redirectErrorStream(true);
+                Process p = builder.start();
+                BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String line;
+                while (true) {
+                    line = r.readLine();
+                    if (line == null) { break; }
+                    System.out.println(line);
+                }
+
 
     }
 }
